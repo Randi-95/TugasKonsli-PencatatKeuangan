39cbc40601779347e5a6d45a626403d194f79625
Randi-95 <randiganteng9528@gmail.com>
2025-08-29 06:43:04 +0700

    finishing style css

--- script.js
+++ script.js
@@ -56,10 +56,23 @@ document.querySelector("form[name=formTambahTransaksi]").onsubmit = function(eve
             <td class="isiJumlah">${inputJumlah}</td>
             <td class="btnAksi">
                 <div class="btnEdit"> 
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#3b82f6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" aria-labelledby="title-edit" role="img"> <title id="title-edit">Edit transaksi</title> <path d="M12 20h9"/> <path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4 12.5-12.5z"/> </svg> 
+                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" 
+                        viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="1.5" 
+                        stroke-linecap="round" stroke-linejoin="round" aria-labelledby="title-edit" role="img"> 
+                    <title id="title-edit">Edit transaksi</title> 
+                    <path d="M12 20h9"/> 
+                    <path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4 12.5-12.5z"/> 
+                    </svg>
+
                 </div>
                 <div class="btnHapus">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ef4444" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" aria-labelledby="title-trash" role="img"> <title id="title-trash">Hapus transaksi</title> <path d="M3 6h18M8 6v14a2 2 0 0 0 2 2h4a2 2 0 0 0 2-2V6M10 10v8M14 10v8M9 6V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v2"/> </svg> 
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" 
+                        viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="1.5" 
+                        stroke-linecap="round" stroke-linejoin="round" aria-labelledby="title-trash" role="img"> 
+                    <title id="title-trash">Hapus transaksi</title> 
+                    <path d="M3 6h18M8 6v14a2 2 0 0 0 2 2h4a2 2 0 0 0 2-2V6M10 10v8M14 10v8M9 6V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v2"/> 
+                    </svg>
+
                  </div>
             </td>
         `
